cfec5f85e85bad4132f5f9a4666a2edb54693d44
wrum241 <kalman_volokh@mail.ru>
2022-11-20 15:33:34 +0600

    ООП приватные и защищенные атрибуты у функций классов

--- OOP_incapsul_nasled.py
+++ OOP_incapsul_nasled.py
@@ -8,25 +8,47 @@ class Chelovek:
     def can_eat(self):
         print('я умею кушать')
     def has_rights(self):
-        self._rights = 'имеет свои права'
+        self._prava = 'имеет свои права'
     def can_poo(self):
         self.__x = 'может пукать'
         self.__y = 'может пукать жидко'
+    def __init__(self):
+        self.__attr = None
+    def __method(self):
+        print('я приватныйватный')
     pass
+
 
 class Architect(Chelovek):
     def can_build(self):
         print('я умею строить')
+
     def can_build_house(self):
         self._woodenhouse = 'деревянный дом'
         self.__papperhouse = 'бумажный домик'
+
+    def __method(self):
+        print('ого как')
+
+    def just(self):
+        self.__method()
+
+    def __init__(self):
+        super().__init__()
+        self.__attr = 'menyau kaliny'
+        print(self.__attr)
     pass
+
+asd = Architect()
+
 
 class Architect_3cat(Architect):
     def help_architect(self):
         print('я помогаю главному архитектору')
+
     def pappers_bringer(self):
         print('я подношу бумажки главному архитектору')
+
     def while_work(self):
         self._otlichnorabotaet = 'качественно выполняющий работу сотрудник'
         self.__lazybaby = 'неторопливый сотрудник'
@@ -34,13 +56,19 @@ class Architect_3cat(Architect):
 class Doctor(Chelovek):
     def can_heal(self):
         print('я умею лечить')
+
     def always_at_work(self):
         self._work_quality = 'много работает'
         self.__podcherk = 'неразборчиво пишет'
+
+    def __init__(self):
+        super().__init__()
+        self.attr = 'chegototam'
     pass
 
 class Okoolist(Doctor):
     def can_heal_eyes(self):
         print('я умею лечить глаза')
+
     pass
 
